19b0e1d5141d1a64b5f514f8223363a72ccac3ca
dharanSdc <schoolpostbox@gmail.com>
2024-09-12 21:47:09 +0530

    Changed something

--- js/gallary.js
+++ js/gallary.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var galleryOne = $("#galleryOne");
   var seeMore1 = $("#seeMore1");
   var data = ``;
-  var limit = 8;
-  var seeMoreCount = 8;
+  var limit = 60;
+  var seeMoreCount = 60;
   var cd = limit;
   var numOfImg = 0;
   var PotFilterOff = true;
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 
   for (var i = 0; i < mainData.length; i++) {
-    if(i === limit ) break;
+    if (i === limit) break;
     data += `
       <a data-cp="${mainData[i].cp}" 
          href="${mainData[i].src}">
@@ -22,131 +22,131 @@ $(document).ready(function(){
     numOfImg = i;
 
     var zx = itemsArray.findIndex(
-      function(item, i) {
-      return item == i;
-    });
+      function (item, i) {
+        return item == i;
+      });
 
-    if(zx == -1){
+    if (zx == -1) {
       itemsArray.push(mainData[i].id);
     }
   }
   galleryOne.html(data);
-  $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
+  $('.gallery a').simpleLightbox({ captionAttribute: 'data-cp' });
 
 
-  seeMore1.click(function(){
-    if(PotFilterOff){
+  seeMore1.click(function () {
+    if (PotFilterOff) {
       cd += limit;
       if (cd - limit == limit) {
         var x = cd - limit;
-      }else {var x = cd - limit +1; numOfImg += 1;}
+      } else { var x = cd - limit + 1; numOfImg += 1; }
 
       for (var i2 = x; i2 < mainData.length; i2++) {
-          if(cd == numOfImg) break;
-          data += `
+        if (cd == numOfImg) break;
+        data += `
             <a data-cp="${mainData[i2].cp}" 
                href="${mainData[i2].src}">
               <img src="${mainData[i2].tum}">
             </a>
           `;
 
-          var zx = itemsArray.findIndex(
-          function(item, i2) {
+        var zx = itemsArray.findIndex(
+          function (item, i2) {
             return item == i2;
           });
 
-          if(zx == -1){
-            itemsArray.push(mainData[i2].id);
-          }
-          numOfImg += 1;
-      }
-    }else {
-      cd+=limit;
-        var subMainData = mainData.filter(
-        function(item) {
-            return item.type == PText;
-        });
-       
-        if(numOfImg == subMainData.length){
-           seeMore1.text("That's it!");
+        if (zx == -1) {
+          itemsArray.push(mainData[i2].id);
         }
+        numOfImg += 1;
+      }
+    } else {
+      cd += limit;
+      var subMainData = mainData.filter(
+        function (item) {
+          return item.type == PText;
+        });
+
+      if (numOfImg == subMainData.length) {
+        seeMore1.text("That's it!");
+      }
 
       for (var i5 = 0; i5 < mainData.length; i5++) {
-          if(cd == numOfImg) break;
-          var id = mainData[i5].id;
-          var zx = itemsArray.findIndex(
-          function(item) {
+        if (cd == numOfImg) break;
+        var id = mainData[i5].id;
+        var zx = itemsArray.findIndex(
+          function (item) {
             return item === id;
           });
-          if (zx !== -1) continue;
-          if (PText != "All") if(mainData[i5].type != PText) continue;
-          itemsArray.push(id);
-          data += `
+        if (zx !== -1) continue;
+        if (PText != "All") if (mainData[i5].type != PText) continue;
+        itemsArray.push(id);
+        data += `
             <a data-cp="${mainData[i5].cp}" 
                href="${mainData[i5].src}">
               <img src="${mainData[i5].tum}">
             </a>
           `;
-          numOfImg += 1;
+        numOfImg += 1;
       }
     }
 
-    if(mainData.length == numOfImg || mainData.length < numOfImg){
-       seeMore1.text("That's it!");
+    if (mainData.length == numOfImg || mainData.length < numOfImg) {
+      seeMore1.text("That's it!");
     }
 
     galleryOne.html(data);
-    $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
+    $('.gallery a').simpleLightbox({ captionAttribute: 'data-cp' });
   });
 
-    //filter
-    var potFilter = $(".pot-filter ");
-    potFilter.click(function(e){
-      numOfImg = 0;
-      cd = limit;
-      itemsArray = []; 
-      seeMore1.text("See More");
-      PotFilterOff = false;
-      PText = $(this).html();
+  //filter
+  var potFilter = $(".pot-filter ");
+  potFilter.click(function (e) {
+    numOfImg = 0;
+    cd = limit;
+    itemsArray = [];
+    seeMore1.text("See More");
+    PotFilterOff = false;
+    PText = $(this).html();
 
-      var parent = $(this).parent()[0];
+    var parent = $(this).parent()[0];
 
-      for (var i3 = 0; i3 < $(parent).children().length; i3++) {
-        $($(parent).children()[i3]).removeClass('active-pf');
-      }
-      $(this).addClass("active-pf");
-      galleryOne.fadeTo(100, .1);
+    for (var i3 = 0; i3 < $(parent).children().length; i3++) {
+      $($(parent).children()[i3]).removeClass('active-pf');
+    }
+    $(this).addClass("active-pf");
+    galleryOne.fadeTo(100, .1);
 
-      setTimeout(function(){
-        galleryOne.fadeTo(100, 1);
-        galleryOne.html("");
-        data = "";
+    setTimeout(function () {
+      galleryOne.fadeTo(100, 1);
+      galleryOne.html("");
+      data = "";
 
-        for (var i4 = 0; i4 < mainData.length; i4++) {
-          if(numOfImg === limit ) break;
-          if(mainData[i4].type != PText && PText != "All") continue;
-          data += `
+      for (var i4 = 0; i4 < mainData.length; i4++) {
+        if (numOfImg === limit) break;
+        if (mainData[i4].type != PText && PText != "All") continue;
+        data += `
             <a data-cp="${mainData[i4].cp}" 
                href="${mainData[i4].src}">
               <img src="${mainData[i4].tum}">
             </a>
           `;
-          numOfImg += 1;
-          var id = mainData[i4].id;
-          var zx = itemsArray.findIndex(
-          function(item) {
+        numOfImg += 1;
+        var id = mainData[i4].id;
+        var zx = itemsArray.findIndex(
+          function (item) {
             return item == id;
           });
 
-          if(zx == -1){
-            itemsArray.push(id);
-          }
+        if (zx == -1) {
+          itemsArray.push(id);
         }
-        galleryOne.html(data);
-          $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
-        
+      }
+      galleryOne.html(data);
+      $('.gallery a').simpleLightbox({ captionAttribute: 'data-cp' });
 
-      }, 500);
 
-    });
+    }, 500);
+
+  });
 });
