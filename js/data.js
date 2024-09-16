ad790f6c64a1d7410049d197232805e0bd6bf7e7
dharanSdc <schoolpostbox@gmail.com>
2024-09-16 09:52:52 +0530

    Final the output 16/24

--- js/data.js
+++ js/data.js
@@ -1,154 +1,849 @@
 mainData = [
   {
     id: 1,
-    type: "Photos",
-    src: 'img/gallery/l1.jpg',
-    tum: 'img/gallery/l1.jpg',
-    cp: "2019 Nature"
+    type: "Landscape",
+    src: 'img/land/l1.jpg',
+    tum: 'img/land/l1.jpg',
+    cp: "Land Leaf"
   },
   {
     id: 2,
-    type: "Photos",
-    src: 'img/gallery/l2.jpg',
-    tum: 'img/gallery/l2.jpg',
-    cp: "2020 Sky"
+    type: "Landscape",
+    src: 'img/land/l2.jpg',
+    tum: 'img/land/l2.jpg',
+    cp: "Flower of Beauti"
   },
   {
     id: 3,
-    type: "Photos",
-    src: 'img/gallery/3.jpg',
-    tum: 'img/gallery/3.jpg',
-    cp: "Under The Sea"
+    type: "Landscape",
+    src: 'img/land/l3.jpg',
+    tum: 'img/land/l3.jpg',
+    cp: "Insert of palm"
   },
   {
     id: 4,
-    type: "Design",
-    src: 'img/gallery/l4.jpg',
-    tum: 'img/gallery/l4.jpg',
-    cp: "Hello, candy"
+    type: "Portrait",
+    src: 'img/port/p1.jpg',
+    tum: 'img/port/p1.jpg',
+    cp: "Cut of Leaf"
   },
   {
     id: 5,
-    type: "Photos",
-    src: 'img/gallery/l5.jpg',
-    tum: 'img/gallery/l5.jpg',
-    cp: "Amazing! 2001"
+    type: "Portrait",
+    src: 'img/port/p2.jpg',
+    tum: 'img/port/p2.jpg',
+    cp: "Early SUN"
   },
   {
     id: 6,
-    type: "Photos",
-    src: 'img/gallery/6.jpg',
-    tum: 'img/gallery/6.jpg',
-    cp: "Amazing! 2017"
+    type: "Portrait",
+    src: 'img/port/p3.jpg',
+    tum: 'img/port/p3.jpg',
+    cp: "Create a thing Memories"
   },
   {
     id: 7,
-    type: "Design",
-    src: 'img/gallery/7.jpg',
-    tum: 'img/gallery/7.jpg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l4.jpg',
+    tum: 'img/land/l4.jpg',
+    cp: "Shadow of Light"
   },
   {
     id: 8,
-    type: "Photos",
-    src: 'img/gallery/8.jpg',
-    tum: 'img/gallery/8.jpg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l5.jpg',
+    tum: 'img/land/l5.jpg',
+    cp: "Lamp of Mountain"
   },
   {
     id: 9,
-    type: "Photos",
-    src: 'img/gallery/9.jpg',
-    tum: 'img/gallery/9.jpg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l6.jpg',
+    tum: 'img/land/l6.jpg',
+    cp: "Little camp fire"
   }, {
     id: 10,
-    type: "Design",
-    src: 'img/gallery/10.jpg',
-    tum: 'img/gallery/10.jpg',
-    cp: "Nice Photo"
+    type: "Stuff",
+    src: 'img/personal/s5.jpg',
+    tum: 'img/personal/s5.jpg',
+    cp: "Alone with Rain"
   }, {
     id: 11,
-    type: "Design",
-    src: 'img/gallery/11.jpg',
-    tum: 'img/gallery/11.jpg',
-    cp: "Nice Photo"
+    type: "Stuff",
+    src: 'img/personal/s4.jpg',
+    tum: 'img/personal/s4.jpg',
+    cp: "Beauti of Time"
   },
   {
     id: 12,
-    type: "Photoshop",
-    src: 'img/gallery/12.jpg',
-    tum: 'img/gallery/12.jpg',
-    cp: "Nice Photo"
+    type: "Stuff",
+    src: 'img/personal/s6.jpg',
+    tum: 'img/personal/s6.jpg',
+    cp: "Cut out of Leaf"
   },
   {
     id: 13,
-    type: "Photos",
-    src: 'img/gallery/13.jpg',
-    tum: 'img/gallery/13.jpg',
-    cp: "Nice Photo"
+    type: "Portrait",
+    src: 'img/port/p4.jpg',
+    tum: 'img/port/p4.jpg',
+    cp: "Painting Sun by camera"
   },
   {
     id: 14,
-    type: "Photos",
-    src: 'img/gallery/14.jpg',
-    tum: 'img/gallery/14.jpg',
-    cp: "Nice Photo"
+    type: "Portrait",
+    src: 'img/port/p5.jpg',
+    tum: 'img/port/p5.jpg',
+    cp: "Pond of Bulding"
   },
   {
     id: 15,
-    type: "Photoshop",
-    src: 'img/gallery/15.jpg',
-    tum: 'img/gallery/15.jpg',
-    cp: "Nice Photo"
+    type: "Portrait",
+    src: 'img/port/p6.jpg',
+    tum: 'img/port/p6.jpg',
+    cp: "Karma of Lotus"
   },
+  //
   {
     id: 16,
-    type: "Photos",
-    src: 'img/gallery/16.jpeg',
-    tum: 'img/gallery/16.jpeg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l7.jpg',
+    tum: 'img/land/l7.jpg',
+    cp: "behind of Glass"
   },
   {
     id: 17,
-    type: "Photos",
-    src: 'img/gallery/17.jpeg',
-    tum: 'img/gallery/17.jpeg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l8.jpg',
+    tum: 'img/land/l8.jpg',
+    cp: "Watching of Shadows"
   },
   {
     id: 18,
-    type: "Photos",
-    src: 'img/gallery/18.jpeg',
-    tum: 'img/gallery/18.jpeg',
-    cp: "Nice Photo"
+    type: "Landscape",
+    src: 'img/land/l9.jpg',
+    tum: 'img/land/l9.jpg',
+    cp: "Reflex of Sun"
   },
   {
     id: 19,
-    type: "Photoshop",
-    src: 'img/gallery/p1.jpg',
-    tum: 'img/gallery/p1.jpg',
-    cp: "Nice Photo"
+    type: "Portrait",
+    src: 'img/port/p7.jpg',
+    tum: 'img/port/p7.jpg',
+    cp: "Little Dam"
   },
   {
     id: 20,
-    type: "Photoshop",
-    src: 'img/gallery/p2.jpg',
-    tum: 'img/gallery/p2.jpg',
-    cp: "Nice Photo"
+    type: "Portrait",
+    src: 'img/port/p8.jpg',
+    tum: 'img/port/p8.jpg',
+    cp: "Alphabet Start a Life"
   },
   {
     id: 21,
-    type: "Photoshop",
-    src: 'img/gallery/p3.jpg',
-    tum: 'img/gallery/p3.jpg',
+    type: "Portrait",
+    src: 'img/port/p9.jpg',
+    tum: 'img/port/p9.jpg',
+    cp: "Window of Mountain"
+  },
+  //--porperly functioned
+  {
+    id: 22,
+    type: "Landscape",
+    src: 'img/land/l10.jpg',
+    tum: 'img/land/l10.jpg',
+    cp: "Every lock has KEY"
+  },
+  {
+    id: 23,
+    type: "Landscape",
+    src: 'img/land/l11.jpg',
+    tum: 'img/land/l11.jpg',
+    cp: "Waste always Use"
+  },
+  {
+    id: 24,
+    type: "Landscape",
+    src: 'img/land/l12.jpg',
+    tum: 'img/land/l12.jpg',
+    cp: "Friend like Mountain"
+  },
+  {
+    id: 26,
+    type: "Portrait",
+    src: 'img/port/p10.jpg',
+    tum: 'img/port/p10.jpg',
+    cp: "Always with a bike"
+  },
+  {
+    id: 27,
+    type: "Portrait",
+    src: 'img/port/p11.jpg',
+    tum: 'img/port/p11.jpg',
+    cp: "Parker a Identity"
+  },
+  {
+    id: 28,
+    type: "Portrait",
+    src: 'img/port/p12.jpg',
+    tum: 'img/port/p12.jpg',
+    cp: "Old is Priceless"
+  },
+  {
+    id: 29,
+    type: "Landscape",
+    src: 'img/land/l13.jpg',
+    tum: 'img/land/l13.jpg',
+    cp: "Little thing better"
+  },
+  {
+    id: 30,
+    type: "Landscape",
+    src: 'img/land/l14.jpg',
+    tum: 'img/land/l14.jpg',
+    cp: "Tea is Relationship"
+  },
+  {
+    id: 31,
+    type: "Landscape",
+    src: 'img/land/l15.jpg',
+    tum: 'img/land/l15.jpg',
+    cp: "Origin in Original"
+  }, {
+    id: 32,
+    type: "Stuff",
+    src: 'img/personal/s2.jpeg',
+    tum: 'img/personal/s2.jpeg',
+    cp: "Traveling Memories"
+  }, {
+    id: 34,
+    type: "Stuff",
+    src: 'img/personal/s7.jpg',
+    tum: 'img/personal/s7.jpg',
+    cp: "Always peace in Alone"
+  },
+  {
+    id: 35,
+    type: "Stuff",
+    src: 'img/personal/s3.jpg',
+    tum: 'img/personal/s3.jpg',
+    cp: "Touch a leaf"
+  },
+  {
+    id: 36,
+    type: "Portrait",
+    src: 'img/port/p13.jpg',
+    tum: 'img/port/p13.jpg',
+    cp: "Better a car"
+  },
+  {
+    id: 37,
+    type: "Portrait",
+    src: 'img/port/p14.jpg',
+    tum: 'img/port/p14.jpg',
+    cp: "Humans have face"
+  },
+  {
+    id: 38,
+    type: "Portrait",
+    src: 'img/port/p15.jpg',
+    tum: 'img/port/p15.jpg',
+    cp: "Precious"
+  },
+  //
+  {
+    id: 39,
+    type: "Landscape",
+    src: 'img/land/l16.jpg',
+    tum: 'img/land/l16.jpg',
     cp: "Nice Photo"
   },
   {
-    id: 22,
-    type: "Photoshop",
-    src: 'img/gallery/p4.jpg',
-    tum: 'img/gallery/p4.jpg',
+    id: 40,
+    type: "Landscape",
+    src: 'img/land/l17.jpg',
+    tum: 'img/land/l17.jpg',
     cp: "Nice Photo"
+  },
+  {
+    id: 41,
+    type: "Landscape",
+    src: 'img/land/l18.jpg',
+    tum: 'img/land/l18.jpg',
+    cp: "Nice Photo"
+  },
+  {
+    id: 42,
+    type: "Portrait",
+    src: 'img/port/p16.jpg',
+    tum: 'img/port/p16.jpg',
+    cp: "Medicine of Human"
+  },
+  {
+    id: 43,
+    type: "Portrait",
+    src: 'img/port/p17.jpg',
+    tum: 'img/port/p17.jpg',
+    cp: "Route is Lone"
+  },
+  {
+    id: 44,
+    type: "Portrait",
+    src: 'img/port/p18.jpg',
+    tum: 'img/port/p18.jpg',
+    cp: "Night is Gorgeous"
+  },
+  //--11.27
+  {
+    id: 45,
+    type: "Landscape",
+    src: 'img/land/l19.jpg',
+    tum: 'img/land/l19.jpg',
+    cp: "Nature way"
+  },
+  {
+    id: 46,
+    type: "Landscape",
+    src: 'img/land/l20.jpg',
+    tum: 'img/land/l20.jpg',
+    cp: "Three face"
+  },
+  {
+    id: 47,
+    type: "Landscape",
+    src: 'img/land/l21.jpg',
+    tum: 'img/land/l21.jpg',
+    cp: "Elephant of Cow"
+  },
+  {
+    id: 48,
+    type: "Portrait",
+    src: 'img/port/p19.jpg',
+    tum: 'img/port/p19.jpg',
+    cp: "Sight seeing Leaf"
+  },
+  {
+    id: 49,
+    type: "Portrait",
+    src: 'img/port/p20.jpg',
+    tum: 'img/port/p20.jpg',
+    cp: "Queen of Angel"
+  },
+  {
+    id: 50,
+    type: "Portrait",
+    src: 'img/port/p21.jpg',
+    tum: 'img/port/p21.jpg',
+    cp: "Waste beauti of Leaf"
+  },
+  {
+    id: 51,
+    type: "Landscape",
+    src: 'img/land/l22.jpg',
+    tum: 'img/land/l22.jpg',
+    cp: "Front of Dam"
+  },
+  {
+    id: 52,
+    type: "Landscape",
+    src: 'img/land/l23.jpg',
+    tum: 'img/land/l23.jpg',
+    cp: "Opening a sky"
+  },
+  {
+    id: 53,
+    type: "Landscape",
+    src: 'img/land/l24.jpg',
+    tum: 'img/land/l24.jpg',
+    cp: "Four pillar of Human"
+  }, {
+    id: 54,
+    type: "Stuff",
+    src: 'img/personal/s13.jpg',
+    tum: 'img/personal/s13.jpg',
+    cp: "Spider Leaf"
+  }, {
+    id: 55,
+    type: "Stuff",
+    src: 'img/personal/s12.jpg',
+    tum: 'img/personal/s12.jpg',
+    cp: "Loneling Travel"
+  },
+  {
+    id: 56,
+    type: "Stuff",
+    src: 'img/personal/s1.jpg',
+    tum: 'img/personal/s1.jpg',
+    cp: "Create a Career"
+  },
+  {
+    id: 57,
+    type: "Portrait",
+    src: 'img/port/p22.jpg',
+    tum: 'img/port/p22.jpg',
+    cp: "Dark Leaf"
+  },
+  {
+    id: 58,
+    type: "Portrait",
+    src: 'img/port/p23.jpg',
+    tum: 'img/port/p23.jpg',
+    cp: "Behind of Soul"
+  },
+  {
+    id: 59,
+    type: "Portrait",
+    src: 'img/port/p24.jpg',
+    tum: 'img/port/p24.jpg',
+    cp: "Difference of Human"
+  },
+  //
+  {
+    id: 60,
+    type: "Landscape",
+    src: 'img/land/l25.jpg',
+    tum: 'img/land/l25.jpg',
+    cp: "Ms.Wonder Wallpaper"
+  },
+  {
+    id: 61,
+    type: "Landscape",
+    src: 'img/land/l26.jpg',
+    tum: 'img/land/l26.jpg',
+    cp: "Running Watch"
+  },
+  {
+    id: 62,
+    type: "Landscape",
+    src: 'img/land/l27.jpg',
+    tum: 'img/land/l27.jpg',
+    cp: "Temple of Fire"
+  },
+  {
+    id: 63,
+    type: "Portrait",
+    src: 'img/port/p25.jpg',
+    tum: 'img/port/p25.jpg',
+    cp: "brasil of Throgh"
+  },
+  {
+    id: 64,
+    type: "Portrait",
+    src: 'img/port/p26.jpg',
+    tum: 'img/port/p26.jpg',
+    cp: "Over a plant"
+  },
+  {
+    id: 65,
+    type: "Portrait",
+    src: 'img/port/p27.jpg',
+    tum: 'img/port/p27.jpg',
+    cp: "Planet like a Star"
+  },
+  //11.40
+  {
+    id: 66,
+    type: "Landscape",
+    src: 'img/land/l28.jpg',
+    tum: 'img/land/l28.jpg',
+    cp: "Between a Road"
+  },
+  {
+    id: 67,
+    type: "Landscape",
+    src: 'img/land/l29.jpg',
+    tum: 'img/land/l29.jpg',
+    cp: "Raing rose"
+  },
+  {
+    id: 68,
+    type: "Landscape",
+    src: 'img/land/l30.jpg',
+    tum: 'img/land/l30.jpg',
+    cp: "Air between Soul"
+  },
+  {
+    id: 69,
+    type: "Portrait",
+    src: 'img/port/p28.jpg',
+    tum: 'img/port/p28.jpg',
+    cp: "Multi of Difference"
+  },
+  {
+    id: 70,
+    type: "Portrait",
+    src: 'img/port/p29.jpg',
+    tum: 'img/port/p29.jpg',
+    cp: "Foot into Mountain"
+  },
+  {
+    id: 71,
+    type: "Portrait",
+    src: 'img/port/p30.jpg',
+    tum: 'img/port/p30.jpg',
+    cp: "First let of God"
+  },
+  {
+    id: 72,
+    type: "Landscape",
+    src: 'img/land/l31.jpg',
+    tum: 'img/land/l31.jpg',
+    cp: "Ride of King"
+  },
+  {
+    id: 73,
+    type: "Landscape",
+    src: 'img/land/l32.jpg',
+    tum: 'img/land/l32.jpg',
+    cp: "Big has Small"
+  },
+  {
+    id: 74,
+    type: "Landscape",
+    src: 'img/land/l33.jpg',
+    tum: 'img/land/l33.jpg',
+    cp: "Land of Peace"
+  }, {
+    id: 75,
+    type: "Stuff",
+    src: 'img/personal/s9.jpg',
+    tum: 'img/personal/s9.jpg',
+    cp: "Memories Hands"
+  }, {
+    id: 76,
+    type: "Stuff",
+    src: 'img/personal/s8.jpg',
+    tum: 'img/personal/s8.jpg',
+    cp: "Signature of Poster"
+  },
+  {
+    id: 77,
+    type: "Stuff",
+    src: 'img/personal/s11.jpg',
+    tum: 'img/personal/s11.jpg',
+    cp: "Near Prince"
+  },
+  {
+    id: 78,
+    type: "Portrait",
+    src: 'img/port/p31.jpg',
+    tum: 'img/port/p31.jpg',
+    cp: "Must have three"
+  },
+  {
+    id: 79,
+    type: "Portrait",
+    src: 'img/port/p32.jpg',
+    tum: 'img/port/p32.jpg',
+    cp: "Change of Mood"
+  },
+  {
+    id: 80,
+    type: "Portrait",
+    src: 'img/port/p33.jpg',
+    tum: 'img/port/p33.jpg',
+    cp: "Five Nation"
+  },
+  //
+  {
+    id: 81,
+    type: "Landscape",
+    src: 'img/land/l34.jpg',
+    tum: 'img/land/l34.jpg',
+    cp: "Storm Air"
+  },
+  {
+    id: 82,
+    type: "Landscape",
+    src: 'img/land/l35.jpg',
+    tum: 'img/land/l35.jpg',
+    cp: "Wall of Leaf"
+  },
+  {
+    id: 83,
+    type: "Landscape",
+    src: 'img/land/l36.jpg',
+    tum: 'img/land/l36.jpg',
+    cp: "Ancient!!!!"
+  },
+  {
+    id: 84,
+    type: "Portrait",
+    src: 'img/port/p34.jpg',
+    tum: 'img/port/p34.jpg',
+    cp: "Eights of SHIVA"
+  },
+  {
+    id: 85,
+    type: "Portrait",
+    src: 'img/port/p35.jpg',
+    tum: 'img/port/p35.jpg',
+    cp: "Rock of Leaf"
+  },
+  {
+    id: 86,
+    type: "Portrait",
+    src: 'img/port/p36.jpg',
+    tum: 'img/port/p36.jpg',
+    cp: "Brother of All"
+  },
+  //11.47
+  {
+    id: 89,
+    type: "Landscape",
+    src: 'img/land/l37.jpg',
+    tum: 'img/land/l37.jpg',
+    cp: "---nature---"
+  },
+  {
+    id: 90,
+    type: "Landscape",
+    src: 'img/land/l38.jpg',
+    tum: 'img/land/l38.jpg',
+    cp: "Mansoon of Mountain"
+  },
+  {
+    id: 91,
+    type: "Landscape",
+    src: 'img/land/l39.jpg',
+    tum: 'img/land/l39.jpg',
+    cp: "Sixth of Temple"
+  },
+  {
+    id: 92,
+    type: "Portrait",
+    src: 'img/port/p38.jpg',
+    tum: 'img/port/p38.jpg',
+    cp: "Green of World"
+  },
+  {
+    id: 93,
+    type: "Portrait",
+    src: 'img/port/p37.jpg',
+    tum: 'img/port/p37.jpg',
+    cp: "Ear of Sound"
+  },
+  {
+    id: 94,
+    type: "Portrait",
+    src: 'img/port/p39.jpg',
+    tum: 'img/port/p39.jpg',
+    cp: "Home of God"
+  },
+  {
+    id: 95,
+    type: "Landscape",
+    src: 'img/land/l41.jpg',
+    tum: 'img/land/l41.jpg',
+    cp: "Over a Rain"
+  },
+  {
+    id: 96,
+    type: "Landscape",
+    src: 'img/land/l40.jpg',
+    tum: 'img/land/l40.jpg',
+    cp: "Peak of God"
+  },
+  {
+    id: 97,
+    type: "Landscape",
+    src: 'img/land/l42.jpg',
+    tum: 'img/land/l42.jpg',
+    cp: "Night ligth"
+  },
+  {
+    id: 98,
+    type: "Portrait",
+    src: 'img/port/p40.jpg',
+    tum: 'img/port/p40.jpg',
+    cp: "Sun in Shiva"
+  },
+  {
+    id: 99,
+    type: "Portrait",
+    src: 'img/port/p41.jpg',
+    tum: 'img/port/p41.jpg',
+    cp: "Sun of light"
+  },
+  {
+    id: 100,
+    type: "Portrait",
+    src: 'img/port/p42.jpg',
+    tum: 'img/port/p42.jpg',
+    cp: "Miss out of Leaf"
+  },
+
+  {
+    id: 101,
+    type: "Landscape",
+    src: 'img/land/l43.jpg',
+    tum: 'img/land/l43.jpg',
+    cp: "Early Ride"
+  },
+  {
+    id: 102,
+    type: "Landscape",
+    src: 'img/land/l44.jpg',
+    tum: 'img/land/l44.jpg',
+    cp: "STAR in Cycle"
+  },
+  {
+    id: 103,
+    type: "Landscape",
+    src: 'img/land/l45.jpg',
+    tum: 'img/land/l45.jpg',
+    cp: "Red Sky"
+  },
+  {
+    id: 104,
+    type: "Portrait",
+    src: 'img/port/p43.jpg',
+    tum: 'img/port/p43.jpg',
+    cp: "Close Door in Enough"
+  },
+  {
+    id: 105,
+    type: "Portrait",
+    src: 'img/port/p44.jpg',
+    tum: 'img/port/p44.jpg',
+    cp: "Fountain of water"
+  },
+  {
+    id: 106,
+    type: "Portrait",
+    src: 'img/port/p45.jpg',
+    tum: 'img/port/p45.jpg',
+    cp: "Balaji behind"
+  },
+
+  {
+    id: 107,
+    type: "Landscape",
+    src: 'img/land/l46.jpg',
+    tum: 'img/land/l46.jpg',
+    cp: "Kingdom of Cholas"
+  },
+  {
+    id: 108,
+    type: "Landscape",
+    src: 'img/land/l47.jpg',
+    tum: 'img/land/l47.jpg',
+    cp: "One of Brother's Home"
+  },
+  {
+    id: 109,
+    type: "Landscape",
+    src: 'img/land/l48.jpg',
+    tum: 'img/land/l48.jpg',
+    cp: "Power of Lassiness"
+  },
+  {
+    id: 110,
+    type: "Portrait",
+    src: 'img/port/p46.jpg',
+    tum: 'img/port/p46.jpg',
+    cp: "Seven Sound-!"
+  },
+  {
+    id: 111,
+    type: "Portrait",
+    src: 'img/port/p47.jpg',
+    tum: 'img/port/p47.jpg',
+    cp: "Blue is Not"
+  },
+  {
+    id: 112,
+    type: "Portrait",
+    src: 'img/port/p48.jpg',
+    tum: 'img/port/p48.jpg',
+    cp: "--Foot is Human Face-!"
+  },
+  {
+    id: 113,
+    type: "Landscape",
+    src: 'img/land/l49.jpg',
+    tum: 'img/land/l49.jpg',
+    cp: "Before a Sun"
+  },
+  {
+    id: 114,
+    type: "Landscape",
+    src: 'img/land/l50.jpg',
+    tum: 'img/land/l50.jpg',
+    cp: "Jail has different Phase"
+  },
+  {
+    id: 115,
+    type: "Landscape",
+    src: 'img/land/l52.jpg',
+    tum: 'img/land/l52.jpg',
+    cp: "Ocean is Peace"
+  }, {
+    id: 116,
+    type: "Portrait",
+    src: 'img/port/p49.jpg',
+    tum: 'img/port/p49.jpg',
+    cp: "dark air in Water"
+  }, {
+    id: 117,
+    type: "Portrait",
+    src: 'img/port/p50.jpg',
+    tum: 'img/port/p50.jpg',
+    cp: "Rich Man Still Live"
+  },
+  {
+    id: 118,
+    type: "Portrait",
+    src: 'img/port/p51.jpg',
+    tum: 'img/port/p51.jpg',
+    cp: "Green is Not Word is Nature"
+  },
+  {
+    id: 119,
+    type: "Landscape",
+    src: 'img/land/l51.jpg',
+    tum: 'img/land/l51.jpg',
+    cp: "Cholas World"
+  },
+  {
+    id: 120,
+    type: "Landscape",
+    src: 'img/land/l53.jpg',
+    tum: 'img/land/l53.jpg',
+    cp: "Temple of 12feet"
+  },
+  {
+    id: 121,
+    type: "Landscape",
+    src: 'img/land/l54.jpg',
+    tum: 'img/land/l54.jpg',
+    cp: "Gate of entry"
+  },
+  //11.59
+  {
+    id: 122,
+    type: "Portrait",
+    src: 'img/port/p52.jpg',
+    tum: 'img/port/p52.jpg',
+    cp: "Dark of Lone"
+  }, {
+    id: 123,
+    type: "Portrait",
+    src: 'img/port/p54.jpg',
+    tum: 'img/port/p54.jpg',
+    cp: "Still Have Water"
+  },
+  {
+    id: 124,
+    type: "Portrait",
+    src: 'img/port/p55.jpg',
+    tum: 'img/port/p55.jpg',
+    cp: "Walk into soul"
+  }, {
+    id: 125,
+    type: "Stuff",
+    src: 'img/personal/s10.jpg',
+    tum: 'img/personal/s10.jpg',
+    cp: "Ring of crush"
   }
+
 ];
